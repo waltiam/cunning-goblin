25322eca43fef4dcf8aaa3676645de86842395f7
walt <2963975+waltiam@users.noreply.github.com>
2025-09-08 16:50:24 +0000

    fix ci

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1>You <em>almost</em> did it!</h1>
   <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the documentation
   </p>
 </template>
 
